a463d633cc0f919626ec05e2da36197ddaf26050
CodeGoy <144829693+CodeGoy@users.noreply.github.com>
2026-08-07 22:47:13 -0400

    merge lists on load

--- load.js
+++ load.js
@@ -4,16 +4,33 @@ console.log("starting load.js:", browser.runtime.getManifest().name, browser.run
 const blockDataKey = "block_data";
 const whitelistDataKey = "whitelist";
 
+let getBlockedList = async () => {
+    let bul = await browser.storage.local.get({ [blockDataKey]: [] });
+    return Object.values(bul)[0]
+}
+
+let getWhitelist = async () => {
+    let bul = await browser.storage.local.get({ [whitelistDataKey]: [] });
+    return Object.values(bul)[0]
+}
+
+let merge = async (array0, array1) => {
+    return [...new Set([...array0, ...array1])];
+}
+
 document.getElementById('file_picker').addEventListener('input', (event) => {
-    // TODO : add list to existing list, loop over lists and merge
     const file = event.target.files[0];
     if (!file) return;
     const reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = async (e) => {
         try {
             let jsonResult = JSON.parse(e.target.result);
-            browser.storage.local.set({[blockDataKey]: jsonResult[blockDataKey]});
-            browser.storage.local.set({[whitelistDataKey]: jsonResult[whitelistDataKey]});
+            let currentBlocklist = await getBlockedList();
+            let newBlockList = await merge(currentBlocklist, jsonResult[blockDataKey]);
+            let currentWhitelist = await getWhitelist();
+            let newWhitelist = await merge(currentWhitelist, jsonResult[whitelistDataKey]);
+            browser.storage.local.set({[blockDataKey]: newBlockList});
+            browser.storage.local.set({[whitelistDataKey]: newWhitelist});
             window.close()
         } catch (error) {
             console.error('Invalid JSON structure. Please check the file formatting.', error);
